87843f18a18a4831b6a9cabb337d0d84a78a1f05
jai dhuria <jaidhuria002@gmail.com>
2025-12-31 22:22:08 +0530

    Buy and sell stocks once

---
+++ Arrays/Day-6/buystocks.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumProfit(int prices[]) {
+        int min = prices[0];
+        int max = 0;
+        for(int i = 0;i<prices.length;i++){
+            min=Math.min(min,prices[i]);
+            max =Math.max(max,prices[i]-min);
+            
+        }
+        return max;
+        
+        
+    }
+}
